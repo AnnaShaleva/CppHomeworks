2a906c4015348b285f11f041e477546486390602
AnnaShaleva <anna.shaleva98@mail.ru>
2018-11-05 19:15:38 +0300

    Homework 5: edited Merge Sort algorythm

--- Homework_5/Sort/MergeSort.cpp
+++ Homework_5/Sort/MergeSort.cpp
@@ -1,163 +1,56 @@
 #include <vector>
 #include <iostream>
 
-template <class InputIterator, class OutputIterator>
-void Merge(OutputIterator result, InputIterator array, int l, int m, int r)
+template <class InputIterator>//, class OutputIterator>
+void Merge(InputIterator l, InputIterator m, InputIterator r)
 {
-	int i, j, k;
-	int n1 = m - l + 1;
-	int n2 = r - m;
-
-	InputIterator L = array + l;
-	InputIterator R = array + m + 1;
-
-	i = 0;
-	j = 0;
-	k = l;
-	while(i < n1 && j < n2)
+	typedef typename std::iterator_traits<InputIterator>::value_type T;
+	std::vector<T> result;
+	InputIterator L = l;
+	InputIterator R = m;
+       	while(L < m && R < r)
 	{
-		if(L[i] <= R[j])
+		if(*L <= *R)
 		{
-			std::cout << "Bla" << std::endl;
-			result[k] = L[i];
-			i++;
-		}
-		else
-		{
-			result[k] = R[j];
-			j++;
-		}
-		k++;
-	}
-	while(i < n1)
-	{
-		result[k] = L[i];
-		i++;
-		k++;
-	}
-	while(j < n2)
-	{
-		result[k] = R[j];
-		j++;
-		k++;
-	}
-}
-	/*
-	std::cout << "Merge:" << std::endl;
-	std::cout << "L = " << *l_init << std::endl;
-	std::cout << "M = " << *m_init << std::endl;
-	std::cout << "R = " << *r_init << std::endl;
-	InputIterator L = l_init;
-	InputIterator R = m_init++;
-	InputIterator n1 = m_init++;
-	InputIterator n2 = r_init++;
-	while (L < n1  && R < n2)
-	{
-		if (*L < *R)
-		{
-			*result = *L;
-			result ++;
+			result.push_back(*L);
 			L++;
 		}
 		else
 		{
-			*result = *R;
-			result++;
+			result.push_back(*R);
 			R++;
 		}
 	}
-	while (L < n1)
+	while(L < m)
 	{
-		*result = *L;
-		result++;
+		result.push_back(*L);
 		L++;
 	}
-	while (R < n2)
+	while(R < r)
 	{
-		*result = *R;
-		result++;
+		result.push_back(*R);
 		R++;
 	}
+	std::copy(result.begin(), result.end(), l);
 }
-	
-	std::cout << "Merge" << std::endl;
-	std::cout << "l = " << *l << std::endl;
-	std::cout << "m = " << *m << std::endl;
-	std::cout << "r = " << *r << std::endl;
-	auto n1 = m - l + 1;
-	auto n2 = r - m;
-
-	InputIterator left, right;
-
-	InputIterator i, j, k;
-
-	while(i < n1 && j < n2)
-	{
-		if (*l <= *(m+1))
-		{
-			*result[k] = *l
-	while(l != m)
-	{
-
-	InputIterator l = l_init;
-	InputIterator m = m_init;
-	InputIterator r = r_init;
-	while(l < m && m < r)
-	{
-		std::cout << "While l < m && m < r" << std::endl;
-		if (*l < *m)
-		{
-			*result = *l;
-			result++;
-			l++;
-		}
-		else
-		{
-			*result = *m;
-			result++;
-			m++;
-		}
-	}
-	while(l < m)
-	{
-		std::cout << "While l < m" << std::endl;
-		*result = *l;
-		result++;
-		l++;
-	}
-	while(m < r)
-	{
-		std::cout << "While m < r" << std::endl;
-		*result = *m;
-		result++;
-		m++;
-	}
-}
-*/
-template <class InputIterator, class OutputIterator>
-void MergeSort(OutputIterator result, InputIterator array, int l, int r)
+template <class InputIterator>//, class OutputIterator>
+void MergeSort(InputIterator l, InputIterator r)
 {
-	//std::cout << "Merge sort start" << std::endl;
-	//std::cout << "l = " << *l << std::endl;
-	//std::cout << "r = " << *r << std::endl;
-	if (l < r)
+	if (l+1 < r)
 	{
-		int m = l + (r - l) / 2;
-		//std::cout << "m = " << *m << std::endl;
-		MergeSort(result, array, l, m);
-		MergeSort(result, array, m + 1, r);
-		Merge(result, array, l, m, r);
-
+		InputIterator m = l + (r - l) / 2;
+		MergeSort(l, m);
+		MergeSort(m, r);
+		Merge(l, m, r);
 	}
 }
 int main()
 {
 	std::vector<int> v{8, 4, 1, 9, 16, 3};
-	std::vector<int> result(v.size());
-	MergeSort(result.begin(), v.begin(), 0, v.size() - 1);
-	for (int i = 0; i < result.size(); i++)
+	MergeSort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++)
 	{
-		std::cout << result[i] << std::endl;
+		std::cout << v[i] << std::endl;
 	}	
 	return 0;
 }
