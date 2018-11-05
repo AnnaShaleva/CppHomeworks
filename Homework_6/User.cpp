03a43fb79546ec37026ae48d5cd4e5a350be405b
AnnaShaleva <anna.shaleva98@mail.ru>
2018-11-05 20:15:07 +0300

    Homework 6: implemented User class with hash<User> specialization

---
+++ Homework_6/User.cpp
@@ -0,0 +1,44 @@
+namespace My
+{
+	class User
+	{
+		int Id;
+		std::string Name;
+		std::string Surname;
+public:
+		User(int id, std::string name, std::string surname)
+		{
+			Id = id;
+			Name = name;
+			Surname = surname;
+		}
+		
+		User(const User& user)
+		{
+			Id = user.Id;
+			Name = user.Name;
+			Surname = user.Surname;
+		}
+
+		bool operator==(const User& user) const
+		{
+			return (Id = user.Id
+				&& Name = user.Name
+				&& Surname == user.Surname);
+		}
+	};
+}
+
+namespace std
+{
+	template<>
+	class hash<My::User>
+	{
+		std::size_t operator() (const My::User& x) const
+		{
+			return ((std::hash<int>()(x.Id)
+				^ (std::hash<std::string>()(x.Name) << 1)) >> 1
+				^ std::hash<std::string>()(x.Surname) << 1);
+		}
+	};
+}	
