4aec4554e86e5bf5b8da2e31b93916179b4c54b8
AnnaShaleva <anna.shaleva98@mail.ru>
2018-11-07 12:11:02 +0300

    Modified tests for Guard

--- Homework_4/Guard/MyGuard_test.cpp
+++ Homework_4/Guard/MyGuard_test.cpp
@@ -6,6 +6,7 @@ struct Guard_param
 {
 	char FillCh;
 	int Width;
+	std::ios_base::fmtflags Flags;
 };
 
 class MyGuard_test: public ::testing::TestWithParam<Guard_param> {};
@@ -16,16 +17,19 @@ TEST_P(MyGuard_test, MyGuard_correct_work_test)
 	const Guard_param& params = GetParam();
 	std::cout.fill(params.FillCh);
 	std::cout.width(params.Width);
+	std::cout.flags(params.Flags);
 	MyGuard* pt = new MyGuard(std::cout);
 	
 	//Act
 	std::cout.fill('+');
 	std::cout.width(5);
+	std::cout.flags(std::ios::right);
 	delete pt;
 	
 	//Assert
 	EXPECT_EQ(params.FillCh, std::cout.fill());
 	EXPECT_EQ(params.Width, std::cout.width());
+	EXPECT_EQ(params.Flags, std::cout.flags());
 }
 
 INSTANTIATE_TEST_CASE_P(
@@ -34,11 +38,13 @@ INSTANTIATE_TEST_CASE_P(
 		::testing::Values(
 			Guard_param{
 			'-',
-			5
+			7,
+			std::ios::right
 			},
 			Guard_param{
 			' ',
-			10
+			10,
+			std::ios::right
 			}
 			)
 		);
