564e93dc737d8135a3bcf3f03611b9ee6f2805da
AnnaShaleva <anna.shaleva98@mail.ru>
2018-11-15 00:03:10 +0300

    Final fixes at MyVector class: added move constructor and move assignment operator

--- Homework_2/MyVector.hh
+++ Homework_2/MyVector.hh
@@ -3,37 +3,38 @@
 template <class T>
 class MyVector
 {
+public:
 	T* beginPointer;
-	const size_t maxSize = 10;
+	const size_t maxSize = 100;
 	size_t currentSize;
 
 public:
-	explicit MyVector() : 
-		currentSize(0),
-		beginPointer(new T[maxSize])
+	explicit MyVector()
 	{
+		currentSize = 0;
+		beginPointer = new T[maxSize];
 	}
 
-	explicit MyVector(size_t count, const T& value) : 
-		currentSize(0),
-		beginPointer(new T[maxSize])
+	explicit MyVector(size_t count, const T& value)
 	{
 		if(count <= maxSize)
 		{
+			currentSize = 0;
+			beginPointer = new T[maxSize];	
 			for (size_t i = 0; i < count; i++)
 			{
 				beginPointer[i] = value;
-				currentSize++;
+				currentSize ++;
 			}
 		}
 	}
 
-	explicit MyVector(size_t count) : 
-		currentSize(0),
-		beginPointer(new T[maxSize])
+	explicit MyVector(size_t count)
 	{
 		if(count <= maxSize)
 		{
+			currentSize = 0;
+			beginPointer = new T[maxSize];
 			for(size_t i = 0; i < count; i++)
 			{
 				beginPointer[i] = T();
@@ -42,20 +43,13 @@ public:
 		}
 	}
 
-	MyVector(const MyVector& other) :
-		currentSize(0),
-		beginPointer(new T[maxSize])
+	MyVector(const MyVector& other)
 	{
-		for(size_t i = 0; i < other.currentSize; i++)
-		{
-			beginPointer[i] = other[i];
-			currentSize++;
-		}
+		currentSize = 0;
+		_Swap(other);	
 	}
 
-	MyVector(MyVector&& other) :
-		currentSize(0),
-		beginPointer(nullptr)
+	MyVector(MyVector&& other)
 	{
 		currentSize = other.currentSize;
 		beginPointer = other.beginPointer;
@@ -68,7 +62,8 @@ public:
 		if(this!= &other)
 		{
 			delete[] beginPointer;
-			Swap(other);
+			if(currentSize == other.currentSize)
+				_Swap(other);
 		}
 		return *this;
 	}
@@ -78,8 +73,9 @@ public:
 		if(this!= &other)
 		{
 			delete[] beginPointer;
-			Swap(other);
-			delete[] other.beginPointer;
+			if(this.currentSize = other.currentSize)
+				this.begiinPointer = other.beginPointer;
+			other.beginPointer = nullptr;
 			other.currentSize = 0;
 		}
 		return *this;
@@ -96,6 +92,11 @@ public:
 		return beginPointer[pos];
 	}
 	
+	T& operator[](size_t pos)
+	{
+		return beginPointer[pos];
+	}
+	
 	T* Begin()
 	{
 		if(beginPointer)
@@ -108,7 +109,7 @@ public:
 	{
 		if(beginPointer)
 		{
-			return *(beginPointer[currentSize]);
+			return &beginPointer[currentSize];
 		}
 	}
 
@@ -120,18 +121,93 @@ public:
 		}
 	}
 
+	void PushBack(const T& value)
+	{
+		if(currentSize < maxSize)
+		{
+			beginPointer[currentSize] = value;
+			currentSize ++;
+		}
+	}
+
+	void PushBack(T&& value)
+	{
+		if(currentSize < maxSize)
+		{
+			beginPointer[currentSize] = std::move(value);
+			currentSize ++;
+			value = T();
+		}
+	}
+
+	void PopBack()
+	{
+		if(currentSize > 0)
+		{
+			beginPointer[currentSize - 1] = T();
+			currentSize --;
+		}
+	} 
+
+	void Swap(MyVector& other)
+	{
+		if(currentSize = other.currentSize)
+		{
+			for(size_t i = 0; i < currentSize; i++)
+			{
+				T temp = beginPointer[i];
+				beginPointer[i] = other[i];
+				other[i] = temp;
+			}
+		}
+	}
+
+	void Erase(int pos)
+	{
+		if(currentSize > 0)
+			if(pos >= 0 && pos < currentSize)
+			{
+				for(int i = pos; i < currentSize - 1; i++)
+					beginPointer[i] = beginPointer[i + 1];
+				beginPointer[currentSize - 1] = T();
+				currentSize --;
+			}
+	}
+
+	void Erase(int first, int last)
+	{
+		if(currentSize > 0)
+			if(first <= last && first >= 0 && last <= currentSize)
+			{
+				if(currentSize - last < last - first)
+				{
+					for(int i = first; i < first + (currentSize-last); i++)
+						beginPointer[i] = beginPointer[i+(last-first)];
+					for(int i = first+(currentSize-last); i < currentSize; i++)
+						beginPointer[i] = T();
+				}
+				else
+				{
+					for(int i = first; i < currentSize - (last-first); i++)
+						beginPointer[i] = beginPointer[i+(last-first)];
+					for(int i = currentSize-(last-first); i < currentSize; i++)
+						beginPointer[i] = T();
+				}
+				currentSize = currentSize - (last - first);
+			}
+	}
+
 private:
 
-	void Swap(MyVector&& other)
+	void _Swap(const MyVector& other)
 	{
-		currentSize = other.currentSize;
+		currentSize = 0;
 		beginPointer = new T[maxSize];
-		for(size_t i = 0; i < currentSize; i++)
+		for(size_t i = 0; i < other.currentSize; i++)
 		{
 			beginPointer[i] = other[i];
+			currentSize++;
 		}
 	}
 		
-
-
 };
