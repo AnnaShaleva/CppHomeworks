a1c3ba3f221ebab2d5d1e47224d8c49a92836096
AnnaShaleva <anna.shaleva98@mail.ru>
2019-01-10 09:27:02 +0300

    Testing

--- Homework_11/test.cpp
+++ Homework_11/test.cpp
@@ -10,5 +10,20 @@ int main()
 {
 	auto t1 = MyThread(MyFoo); 
 	t1.join();
+
+	auto t2 = MyThread();
+	std::cout << t2.get_id() << std::endl;
+	std::cout << "t2 - joinable: " << t2.joinable() << std::endl;
+	
+	auto t3 = MyThread(MyFoo);
+	std::cout << t3.get_id() << std::endl;
+	t2 = MyThread(std::move(t3));
+	std::cout << "t2 - joinable: " << t2.joinable() << std::endl;
+	std::cout << t2.get_id() << std::endl;
+	
+	t2.join();
+	std::cout << "t2 - ojoinable: " << t2.joinable() << std::endl;
+	std::cout << t2.get_id() << std::endl;	
+
 	return 0;
 };
