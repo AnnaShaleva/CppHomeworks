7a97992b7775bbeeaf7388fad31f368acd968080
AnnaShaleva <anna.shaleva98@mail.ru>
2019-01-10 15:34:00 +0300

    Comparing of parallel and sequential sotrs

--- Homework_8/test.cpp
+++ Homework_8/test.cpp
@@ -1,15 +1,26 @@
 #include "MergeSort.hh"
 #include <iostream>
+#include <chrono>
 
 int main()
 {
-	std::vector<int> v1{8, 4, 1, 9, 16, 3};
-	MergeSort(v1.begin(), v1.end(), std::thread::hardware_concurrency());
-	std::vector<int> v2{8, 4, 1, 9, 16, 3};
-	MergeSort(v2.begin(), v2.end(), 1);
-	for (int i = 0; i < v1.size(); i++)
+
+	for(int i = 10; i < 1000000000; i = i * 10)
 	{
-		std::cout << v1[i] << std::endl;
-	}	
+		std::vector<int> v1;
+		for (int j = 0; j < i; j++)
+			v1.push_back(std::rand()%100);
+		std::vector<int> v2 = v1;
+		auto start1 = std::chrono::system_clock::now();
+		MergeSort(v1.begin(), v1.end(), std::thread::hardware_concurrency());
+		auto end1 = std::chrono::system_clock::now();
+		auto start2 = std::chrono::system_clock::now();
+		MergeSort(v2.begin(), v2.end(), 1);
+		auto end2 = std::chrono::system_clock::now();
+		std::cout << "Number of elements: " << i << std::endl;
+		std::cout << "Parallel sort: " << std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1).count() << " ms" << std::endl;
+		std::cout << "Single thread sort: " <<  std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2).count() << " ms" << std::endl;
+		std::cout << "_______" << std::endl;
+	}
 	return 0;
 }
